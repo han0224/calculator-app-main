d1dda241c0219a57313af68eeb205b61a405f120
han0224 <70616579+han0224@users.noreply.github.com>
2022-04-02 15:07:40 +0900

    수정

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const setTheme = (e) =>{
     const target = e.target;
     const body = document.getElementsByTagName('body');
-    body[0].className = target.id
+    if(target.id){
+        console.log(target.id)
+        body[0].className = target.id
+    }
 }
